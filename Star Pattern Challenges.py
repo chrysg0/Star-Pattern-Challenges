ff538fbaf733562630cd6471fffaab330533ab55
chrysg0 <chrysg08@gmail.com>
2025-11-22 09:16:35 -0500

    first commit

--- Star Pattern Challenges.py
+++ Star Pattern Challenges.py
@@ -2,6 +2,8 @@ print("hello")
 
 #challenge one
 
+print("Challenge 1:\n")
+
 def challengeone():
     for i in range(3):
         for i in range(3):
@@ -13,6 +15,8 @@ challengeone()
 
 #challenge two
 
+print("Challenge 2:\n")
+
 for row in range(3):
     for col in range(3):
         if row == 1 and col == 1:
@@ -22,6 +26,8 @@ for row in range(3):
     print("\n")
 
 #challenge three
+
+print("Challenge 3:\n")
 
 for row in range(3):
     for col in range(3):
